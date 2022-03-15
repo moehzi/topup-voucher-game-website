c952e3f56cd016e6156f123f256c8f44604acd0e
moehzi <faizi1234098@gmail.com>
2022-03-15 23:13:35 +0800

    feat: slicing sign-up-photo-success page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import '../styles/transactions.css';
 import '../styles/transactions-detail.css';
 import '../styles/edit-profile.css';
 import '../styles/navbar-log-in.css';
+import '../styles/sign-up-photo-success.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 

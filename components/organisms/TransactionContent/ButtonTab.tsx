500d1d59a4ed4be424935ee79049c85112c21e6b
moehzi <faizi1234098@gmail.com>
2022-03-16 13:37:43 +0800

    refactor: refactor member transaction page

---
+++ components/organisms/TransactionContent/ButtonTab.tsx
@@ -0,0 +1,17 @@
+import cx from 'classnames';
+
+interface ButtonTabProps {
+  title: string;
+  active?: boolean;
+}
+export default function ButtonTab({ title, active }: Partial<ButtonTabProps>) {
+  const btnClass = cx({
+    'btn btn-status rounded-pill text-sm  me-3': true,
+    'btn-active': active,
+  });
+  return (
+    <a data-filter="*" href="#" className={btnClass}>
+      {title}
+    </a>
+  );
+}

a60d955443e11023ad7e007433d4137b34217fad
moehzi <faizi1234098@gmail.com>
2022-03-16 15:57:12 +0800

    refactor: refactor transaction detail page

---
+++ components/organisms/TransactionDetailContent/Row.tsx
@@ -0,0 +1,14 @@
+interface RowProps {
+  label: string;
+  value: string | number;
+  className?: string;
+}
+
+export default function Row({ label, value, className }: Partial<RowProps>) {
+  return (
+    <p className="text-lg color-palette-1 mb-20">
+      {label}
+      <span className={`purchase-details ${className}`}>{value}</span>
+    </p>
+  );
+}

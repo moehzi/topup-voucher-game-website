023c4771b4703b993a50b9110588f0980b389ea7
moehzi <faizi1234098@gmail.com>
2022-03-23 16:11:19 +0800

    feat: add update feature for category page

--- backend/app/category/controller.js
+++ backend/app/category/controller.js
@@ -1,3 +1,4 @@
+const { findOne } = require('./model');
 const Category = require('./model');
 
 module.exports = {
@@ -29,5 +30,34 @@ module.exports = {
     } catch (error) {
       console.log(error);
     }
+  },
+
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const category = await Category.findOne({ _id: id });
+
+      res.render('admin/category/edit', {
+        category,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+
+      const category = await Category.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        { name }
+      );
+      res.redirect('/category');
+    } catch (error) {}
   },
 };
--- backend/app/category/router.js
+++ backend/app/category/router.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const { index, viewCreate, actionCreate } = require('./controller');
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  viewEdit,
+  actionEdit,
+} = require('./controller');
 
 /* GET home page. */
 router.get('/category', index);
 router.get('/category/create', viewCreate);
 router.post('/category/create', actionCreate);
+router.get('/category/edit/:id', viewEdit);
+router.put('/category/edit/:id', actionEdit);
 
 module.exports = router;

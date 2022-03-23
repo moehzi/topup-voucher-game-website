cc6199f0750a1808efa83014cc08ab79e2179957
moehzi <faizi1234098@gmail.com>
2022-03-23 15:27:40 +0800

    feat: add create feature for category page

--- backend/app/category/router.js
+++ backend/app/category/router.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const { index } = require('./controller');
+const { index, viewCreate, actionCreate } = require('./controller');
 
 /* GET home page. */
 router.get('/category', index);
+router.get('/category/create', viewCreate);
+router.post('/category/create', actionCreate);
 
 module.exports = router;

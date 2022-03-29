09db279c999615b1ad38735ba6b46edf25daf444
moehzi <faizi1234098@gmail.com>
2022-03-29 23:01:30 +0800

    feat: add view create voucher page

--- backend/app/voucher/controller.js
+++ backend/app/voucher/controller.js
@@ -1,4 +1,6 @@
 const Voucher = require('./model');
+const Category = require('../category/model');
+const Nominal = require('../nominal/model');
 
 module.exports = {
   index: async (req, res) => {
@@ -12,6 +14,19 @@ module.exports = {
       console.log(alert);
       res.render('admin/voucher/view_voucher', { voucher, alert });
     } catch (err) {
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/voucher');
+    }
+  },
+
+  viewCreate: async (req, res) => {
+    try {
+      const category = await Category.find();
+      const nominal = await Nominal.find();
+
+      res.render('admin/voucher/create', { category, nominal });
+    } catch (error) {
       req.flash('alertMessage', `${err.message}`);
       req.flash('alertStatus', 'danger');
       res.redirect('/voucher');
